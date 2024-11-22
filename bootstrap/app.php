5e1b4aa099756196525a69d030edf446bbe3c450
Ermuun Erdenekhuyag <73881215+ermuunn@users.noreply.github.com>
2024-11-22 11:32:20 +0800

    fix: route fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,6 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
-        health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(JsonMiddleware::class);
@@ -45,7 +44,6 @@ return Application::configure(basePath: dirname(__DIR__))
             return response()->json([
                 'success' => false,
                 'message' => $e->getMessage(),
-                // 'exception' => get_class($e)
             ], 400);
         });
     })->create();
